62cca6a22acc2d65680d1075250a2a5e7ef8cb2f
ark0000 <karthikarun407@gmail.com>
2024-03-04 22:08:21 +0530

    cookie

---
+++ sevs/src/main/java/com/servSessionCookie/DashBoard.java
@@ -0,0 +1,60 @@
+package com.servSessionCookie;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class DashBoard
+ */
+public class DashBoard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashBoard() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw=response.getWriter();
+	
+		Cookie[] cookies=request.getCookies();
+	boolean ssID=false;
+	for(Cookie c:cookies) {
+		String name=c.getName();
+		if(name.equals("sssID")) {
+			ssID=true;
+			pw.print(c.getValue());
+			break;
+		}
+	}
+
+	if(ssID) {
+	 pw.println("COOKIE has ");
+	}
+	else {
+		pw.print("NO COOKIE");
+	}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ sevs/src/main/java/com/servSessionCookie/HomeServlet.java
@@ -0,0 +1,50 @@
+package com.servSessionCookie;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+public class HomeServlet extends HttpServlet {
+	Random rand=new Random();
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		PrintWriter out=response.getWriter();      
+int ssID=rand.nextInt();
+
+out.print(ssID+" home");
+//request.getRequestDispatcher("DashBoard?ssID="+ssID).include(request, response);
+Cookie cookie=new Cookie("sssID", ssID+"");
+response.addCookie(cookie);
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
